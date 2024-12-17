8e66e71fc53ae9759b6cc80f30035a5311319d26
Fauzi Ferdiansyah <43751540+fauziferdi@users.noreply.github.com>
2024-12-17 12:51:53 +0700

    feat : add component Earning Month and Summary component from home page

---
+++ src/components/Home/BestItemSalesComponent.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Items from "../../assets/icons/home/Items.svg";
+
+const BestItemSalesComponent = () => {
+  return (
+    <div class="card-body py-7">
+      <div className="d-flex flex-column">
+        <h1>Best Item</h1>
+        {/* <h1>Best Item Sales</h1>
+        <div className="gap-2 my-3 d-flex justify-items-center align-items-center">
+          <img src={Items} alt="" className="w-1/6" />
+          <div>
+            <h3 className="font-bold">Kids Electric</h3>
+            <p className="mt-2 text-sm text-gray-600">Toys</p>
+          </div>
+          <h3 className="font-bold">
+            <i class="bi bi-chevron-right"></i>
+          </h3>
+        </div> */}
+      </div>
+    </div>
+  );
+};
+
+export default BestItemSalesComponent;
--- src/components/Home/HomeComponent.jsx
+++ src/components/Home/HomeComponent.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React from "react";
+import EarningMonthComponent from "./EarningMonthComponent";
+import SummaryComponent from "./SummaryComponent";
+import RevanueComponent from "./RevenueComponent";
+import BestItemSalesComponent from "./BestItemSalesComponent";
 
 const HomeComponent = () => {
-    return (
-        <div>
-            <h1>Home Component</h1>
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-9">
+          <SummaryComponent />
         </div>
-    )
-}
+        <div className="col-md-3">
+          <EarningMonthComponent />
+        </div>
+      </div>
+      <div className="mt-10 row">
+        <div className="col-md-9">
+          <RevanueComponent />
+        </div>
+        <div className="col-md-3">
+          <div class="card">
+            <BestItemSalesComponent />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default HomeComponent;
