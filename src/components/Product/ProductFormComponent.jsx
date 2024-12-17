f9d6d4933f7de89e360a9954ac0e59b7497914e6
achmadzahrul01 <achmadzahrul01@gmail.com>
2024-12-17 11:16:32 +0700

    feat: Adding sidebar

--- src/components/Product/ProductFormComponent.jsx
+++ src/components/Product/ProductFormComponent.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
-const ProductFormComponent = () => {
+const ProductFormComponent = ({ isEdit, isDetail }) => {
   const { id } = useParams();
   return (
     <div>
