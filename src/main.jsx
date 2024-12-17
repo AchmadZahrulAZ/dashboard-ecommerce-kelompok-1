2c77b3dac8eba57099006444d74fbad64c62ad88
Fauzi Ferdiansyah <43751540+fauziferdi@users.noreply.github.com>
2024-12-17 12:52:59 +0700

    fixed : fixed sidebar container

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 // index.js atau App.js
-import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap
-import './index.css'; // Import Tailwind CSS
-import App from './App.jsx'
+import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap
+import "bootstrap-icons/font/bootstrap-icons.css"; // Import Bootstrap Icons
+import "./index.css"; // Import Tailwind CSS
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 import SidebarComponent from "../components/SidebarComponent";
 import BannerPage from "./BannerPage";
 import ProductPage from "./ProductPage";
@@ -51,37 +56,39 @@ const DashboardWithSidebar = () => {
   };
 
   return (
-    <div className="flex">
-      {/* Pass activePage dynamically */}
+    <div className="flex items-start justify-center lg:justify-between">
       <SidebarComponent activePage={getActivePage()} />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/home" element={<HomePage />} />
+      <div className="flex flex-col w-full h-screen px-3 py-12 bg-backgroundcstm">
+        {/* Pass activePage dynamically */}
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/home" element={<HomePage />} />
 
-        <Route path="/banner" element={<BannerPage />} />
-        <Route path="/banner/add" element={<BannerPage />} />
-        <Route path="/banner/edit/:id" element={<BannerPage />} />
-        <Route path="/banner/detail/:id" element={<BannerPage />} />
+          <Route path="/banner" element={<BannerPage />} />
+          <Route path="/banner/add" element={<BannerPage />} />
+          <Route path="/banner/edit/:id" element={<BannerPage />} />
+          <Route path="/banner/detail/:id" element={<BannerPage />} />
 
-        <Route path="/product" element={<ProductPage />} />
-        <Route path="/product/add" element={<ProductPage />} />
-        <Route path="/product/edit/:id" element={<ProductPage />} />
-        <Route path="/product/detail/:id" element={<ProductPage />} />
+          <Route path="/product" element={<ProductPage />} />
+          <Route path="/product/add" element={<ProductPage />} />
+          <Route path="/product/edit/:id" element={<ProductPage />} />
+          <Route path="/product/detail/:id" element={<ProductPage />} />
 
-        <Route path="/promotion" element={<PromotionPage />} />
-        <Route path="/promotion/add" element={<PromotionPage />} />
-        <Route path="/promotion/edit/:id" element={<PromotionPage />} />
-        <Route path="/promotion/detail/:id" element={<PromotionPage />} />
+          <Route path="/promotion" element={<PromotionPage />} />
+          <Route path="/promotion/add" element={<PromotionPage />} />
+          <Route path="/promotion/edit/:id" element={<PromotionPage />} />
+          <Route path="/promotion/detail/:id" element={<PromotionPage />} />
 
-        <Route path="/stock" element={<StockPage />} />
-        <Route path="/stock/add" element={<StockPage />} />
-        <Route path="/stock/edit/:id" element={<StockPage />} />
-        <Route path="/stock/detail/:id" element={<StockPage />} />
+          <Route path="/stock" element={<StockPage />} />
+          <Route path="/stock/add" element={<StockPage />} />
+          <Route path="/stock/edit/:id" element={<StockPage />} />
+          <Route path="/stock/detail/:id" element={<StockPage />} />
 
-        <Route path="/orders" element={<OrdersPage />} />
-        <Route path="/rating" element={<RatingPage />} />
-        <Route path="/category" element={<CategoryPage />} />
-      </Routes>
+          <Route path="/orders" element={<OrdersPage />} />
+          <Route path="/rating" element={<RatingPage />} />
+          <Route path="/category" element={<CategoryPage />} />
+        </Routes>
+      </div>
     </div>
   );
 };
